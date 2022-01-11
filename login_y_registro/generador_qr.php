70adfd60db3ea92610dd706aad7f745b70a6e94f
amyjubi18 <amygarcia9618@gmail.com>
2022-01-11 01:14:39 -0400

    cambio

--- login_y_registro/generador_qr.php
+++ login_y_registro/generador_qr.php
@@ -1,28 +1,17 @@
 <?php
-    require ('phpqrcode/qrlib.php');
-    if(isset($_POST['generar']))
-    {
-        if(!empty($_POST['cod']))
-        {
-            $dir = 'temp/';
+    require ('C:/Users/Amanda/Desktop/controldeacceso/phpqrcode/qrlib.php');
+     $dir = 'temp/';
+     if(!file_exists($dir))
+     mkdir ($dir);
 
-            if(!file_exists($dir))
-                mkdir ($dir);
-            {
-                $cod = $_POST['cod'];
-                $tam = htmlentities($_POST['tam']);
-                $niv = htmlentities($_POST['niv']);
-                $filename = $dir.'test.png';
-                $marco = 3;
+     $filename = $dir.'test.png';
+     $tamanio = 10;
+     $level ='L';
+     $frameSize =3;
+     $contenido ="25565678";
 
-                QRcode::png($cod, $filename, $niv, $tam, $marco);
+     QRcode::png($contenido, $filename, $level, $tamanio, $frameSize);
     
-                echo '<img src= "'.$filename.'" align = "left" />';
-            }
-            
-            
-            } 
-    
-    }
-    
+              echo '<img src= "'.$dir.basename($filename).'"/><hr/>';
+
 ?>
