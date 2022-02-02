b6d8c09f369dca8d4307e6dfb433d960d671747a
amyjubi18 <amygarcia9618@gmail.com>
2022-02-01 23:04:19 -0400

    actualizar

--- login_y_registro/registro_estudiante_bd.php
+++ login_y_registro/registro_estudiante_bd.php
@@ -1,5 +1,7 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+
 include 'conexion.php';
 // require ('/phpqrcode/qrlib.php');
 $periodo_id = $_POST['periodo_id'];
@@ -63,13 +65,6 @@ if($ejecutar){
 </div>
 <img src="/estilos/img/usuario.png" id="usuario">
 <div class="datos">
-<?php
-$query= mysql_query("SELECT @@identity AS id_estudiantes");
- if ($row = mysql_fetch_row($query)) 
- {
-   $id = trim($row[0]);
- }
-?>
 <h6><b>Periodo: </b><?php echo $periodo_id; ?></h6>
 <h6><b>Nombre: </b><?php echo $nombre_est; ?></h6>
   <h6><b>Apellido: </b><?php echo $apellido_est; ?></h6>
